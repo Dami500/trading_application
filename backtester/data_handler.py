859943c890bc192083d9fea49f3d1ff950a314cd
Dami500 <damilareajayiobe0@gmail.com>
2025-08-14 10:11:17 -0400

    changed name

---
+++ backtester/data_handler.py
@@ -0,0 +1,26 @@
+class DataHandler:
+    """
+    This is an Abstract base Class of the data handler of this trading system.
+    Its purpose is to load prices into a pandas Dataframe then generate
+    these prices one by one sequentially till the end of the trading period.
+    """
+    def __init__(self, symbols, queue):
+        self.symbols = symbols
+        self.latest_symbol_data = {}
+        self.queue = queue
+
+    def load(self)-> None:
+        """
+        This method loads the OHLCV data from any database or data source of
+        the users choosing
+        :return:
+        """
+        raise NotImplementedError
+
+    def generate(self) ->dict:
+        raise NotImplementedError
+
+    def add_market_event(self) -> None:
+        raise NotImplementedError
+
+
