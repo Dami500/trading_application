2bd5467cdffed25676b8794a34dbc6f99aeeb34e
Dami500 <damilareajayiobe0@gmail.com>
2025-08-14 10:19:38 -0400

    update

---
+++ backtester/event.py
@@ -0,0 +1,42 @@
+
+class event:
+    """
+    This class represents the base class for which 
+    all the events in this backtester will be based on
+    """
+    def __init__(self):
+        self.type = None
+
+
+class market_event(event):
+    """
+    This class represents the event that some new data has been introduced into the market.
+    Typically new prices or maybe a news feed
+    """
+    def __init__(self, info):
+        """
+        Initializes the type and info (bar or chunk of information/OHLCV + date)
+        """
+        self.type = "Market"
+        self.info = info
+
+class signal_event(event):
+    """
+    This class represents the signal calculated by the trading strategy it can be a LONG or SHORT signal
+    """
+    def __init__(self, direction, symbols, strategy_id):
+        self.type = "Signal"
+        self.direction = direction
+        self.symbols = symbols
+        self.strategy_id = strategy_id
+
+class order_event(event):
+    """
+    This class represents an order that is to be executed and accounted for by the portfolio handler
+    """
+    def __init__(self, amount, price, date, direction):
+        self.type = "Order"
+        self.direction = direction
+        self.amount = amount
+        self.price = price
+        self.date = date
---
+++ backtester/securities_master_handler.py
@@ -0,0 +1,5 @@
+db_host = 'localhost'
+db_user = 'sec_user'
+db_pass = 'Damilare20$'
+db_name = 'securities_master'
+con = msc.connect(host=db_host, user=db_user, password=db_pass, db=db_name)
